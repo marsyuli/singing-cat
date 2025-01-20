5ee61c4044fcda17edf66326b8297fa63a1f2908
Ksenia <mkseniam11@gmail.com>
2025-01-21 01:25:08 +0300

    development of the main window

--- main window.py
+++ main window.py
@@ -8,10 +8,10 @@ size = width, height = 800, 600
 surface = pygame.display.set_mode(size)
 pygame.display.set_caption('Singing Cat')
 surface.fill('#ffc2cc')
-font = pygame.font.Font('Cat.otf', 120)
+font = pygame.font.Font('fonts/Cat.otf', 120)
 text = font.render('Singing Cat', True, 'yellow')
 surface.blit(text, ((width - text.get_rect()[2]) // 2, 50))
-buttonFont = pygame.font.SysFont('Arial', 28)
+buttonFont = pygame.font.Font('fonts/Collect Em All BB.ttf', 28)
 buttons = []
 class Button:
     def __init__(self, x, y, width, height, buttonText='', function=None, onePress=False):
@@ -62,6 +62,23 @@ def exit():
 Button(100, 230, 600, 80, 'Играть', func)
 Button(100, 320, 600, 80, 'Правила', func)
 Button(100, 410, 600, 80, 'Выйти', exit)
+
+volume_button_image = pygame.image.load('images/volume.png')
+volume_button_image = pygame.transform.scale(volume_button_image, (100, 100))
+volume_button_rect = volume_button_image.get_rect()
+surface.blit(volume_button_image, (690, 490))
+
+mute_button_image = pygame.image.load('images/mute.png')
+mute_button_image = pygame.transform.scale(mute_button_image, (100, 100))
+mute_button_rect = mute_button_image.get_rect()
+
+def volume_button_click():
+    print(1)
+
+pygame.mixer.music.load('music/Angel We Have Heard On High.mp3')
+pygame.mixer.music.play()
+
+
 pygame.display.update()
 
 
@@ -71,6 +88,9 @@ while running:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if volume_button_rect.collidepoint(pygame.mouse.get_pos()):
+                volume_button_click()
     for btn in buttons:
         btn.process()
     pygame.display.flip()
