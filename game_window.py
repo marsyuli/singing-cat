0a1ea32ac90c0b1942108a0cbcb7e6af2fe29b07
Yulia Vankina <yulya.vankina.08@mail.ru>
2025-01-30 18:48:09 +0300

    add kill candy and kill ice_cream

--- game_window.py
+++ game_window.py
@@ -14,6 +14,8 @@ game_screen.fill('#EFCDD6')
 all_sprites = pygame.sprite.Group()
 cat_group = pygame.sprite.Group()
 board_group = pygame.sprite.Group()
+ice_cream_group = pygame.sprite.Group()
+candy_group = pygame.sprite.Group()
 
 
 def load_image(name, colorkey=None):
@@ -45,6 +47,7 @@ class Board(pygame.sprite.Sprite):
 
 class Cat(pygame.sprite.Sprite):
     cat_right_image = pygame.transform.scale(load_image('cat_right.png'), (150, 150))
+    cat_2 = pygame.transform.scale(load_image('cat_mouth_right.png'), (150, 150))
 
     def __init__(self):
         super().__init__(all_sprites, cat_group)
@@ -57,7 +60,7 @@ class Cat(pygame.sprite.Sprite):
     def update(self, event):
         if event.key == pygame.K_LEFT:
             if self.rect.x > 15:
-                self.rect.x -= 10
+                self.rect.x -= 15
             else:
                 if event.key == pygame.K_RIGHT:
                     self.rect.x += 10
@@ -73,7 +76,7 @@ class Ice_cream(pygame.sprite.Sprite):
     ice_cream_image = pygame.transform.scale(load_image('ice_cream.png'), (90, 90))
 
     def __init__(self):
-        super().__init__(all_sprites)
+        super().__init__(all_sprites, ice_cream_group)
         self.image = Ice_cream.ice_cream_image
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
@@ -82,6 +85,27 @@ class Ice_cream(pygame.sprite.Sprite):
     def update(self):
         if not pygame.sprite.collide_mask(self, cat):
             self.rect = self.rect.move(0, 1)
+        else:
+            cat.image = cat.cat_2
+            self.kill()
+
+
+class Candy(pygame.sprite.Sprite):
+    candy_image = pygame.transform.scale(load_image('candy.png'), (70, 70))
+
+    def __init__(self):
+        super().__init__(all_sprites, candy_group)
+        self.image = Candy.candy_image
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.x, self.rect.y = random.randint(15, 370), 0
+
+    def update(self):
+        if not pygame.sprite.collide_mask(self, cat):
+            self.rect = self.rect.move(0, 1)
+        else:
+            cat.image = cat.cat_2
+            self.kill()
 
 
 board = Board()
@@ -95,9 +119,15 @@ while running:
         if event.type == pygame.KEYDOWN:
             cat_group.update(event)
         if event.type == pygame.MOUSEBUTTONDOWN:
-            Ice_cream()
+            candy_or_ice_cream = random.choice(['ice_cream', 'candy'])
+            if candy_or_ice_cream == 'ice_cream':
+                Ice_cream()
+            else:
+                Candy()
     game_screen.fill('#EFCDD6')
     all_sprites.draw(game_screen)
-    clock.tick(50)
+    candy_group.update()
+    ice_cream_group.update()
+    clock.tick(40)
     pygame.display.flip()
 pygame.quit()
